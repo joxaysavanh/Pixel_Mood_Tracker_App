ae3f55c4a5968b82b98813fbf7bcf44e514a827f
Xaysavanh-jojo <155747662+Xaysavanh-jojo@users.noreply.github.com>
2026-06-04 22:25:16 +0700

    Update main dart for Hive database

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'app.dart';
 
-void main() {
+void main() async {
+  // Force flutter to get ready for Database
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Start Hive in that platform
+  await Hive.initFlutter();
+
+  // Open box of data store
+  await Hive.openBox('mood_box');
+
   runApp(const ProviderScope(child: MoodTrackerApp()));
 }

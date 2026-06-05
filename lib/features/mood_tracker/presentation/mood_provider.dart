cc868a521598f738454d7a2c1dd49a7fcd118174
Xaysavanh-jojo <155747662+Xaysavanh-jojo@users.noreply.github.com>
2026-06-05 21:04:48 +0700

    Add Hive Database

--- lib/features/mood_tracker/presentation/mood_provider.dart
+++ lib/features/mood_tracker/presentation/mood_provider.dart
@@ -1,12 +1,33 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:pixel_mood_tracker/core/constants/app_colors.dart';
 
 // StateNotifier, Define State is Map<int, Moodtype> (key is day, value is mood)
 class MoodNotifier extends StateNotifier<Map<int, MoodType>> {
-  MoodNotifier() : super({});
+  // use Hive box
+  final _box = Hive.box('mood_box');
 
-  // Update mood of each day function
+  // When create MoodNotifier, go to download the old data immediately
+  MoodNotifier() : super({}) {
+    _loadMoodsFromDatabase();
+  }
+
+  // Load old data from database function
+  void _loadMoodsFromDatabase() {
+    final Map<int, MoodType> loadedMoods = {};
+    for (var key in _box.keys) {
+      if (key is int) {
+        final String? moodName = _box.get(key);
+        if (moodName != null) {
+          loadedMoods[key] = MoodType.values.byName(moodName);
+        }
+      }
+    }
+    state = loadedMoods;
+  }
+
   void updateMood(int day, MoodType mood) {
+    _box.put(day, mood.name);
     state = {...state, day: mood};
   }
 }

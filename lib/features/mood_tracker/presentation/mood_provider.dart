f2d7e0d6495f0e0b53c077a31e8a2fa5d35c0878
Xaysavanh-jojo <155747662+Xaysavanh-jojo@users.noreply.github.com>
2026-06-06 02:22:38 +0700

    Add Note record on each day

--- lib/features/mood_tracker/presentation/mood_provider.dart
+++ lib/features/mood_tracker/presentation/mood_provider.dart
@@ -2,38 +2,62 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:pixel_mood_tracker/core/constants/app_colors.dart';
 
+class DayRecord {
+  final MoodType mood;
+  final String note;
+
+  DayRecord({required this.mood, required this.note});
+}
+
 // StateNotifier, Define State is Map<int, Moodtype> (key is day, value is mood)
-class MoodNotifier extends StateNotifier<Map<int, MoodType>> {
+class MoodNotifier extends StateNotifier<Map<int, DayRecord>> {
   // use Hive box
   final _box = Hive.box('mood_box');
 
   // When create MoodNotifier, go to download the old data immediately
   MoodNotifier() : super({}) {
-    _loadMoodsFromDatabase();
+    _loadDataFromHive();
   }
 
   // Load old data from database function
-  void _loadMoodsFromDatabase() {
-    final Map<int, MoodType> loadedMoods = {};
+  void _loadDataFromHive() {
+    final Map<int, DayRecord> loadedData = {};
     for (var key in _box.keys) {
       if (key is int) {
-        final String? moodName = _box.get(key);
+        // Hive จะเก็บข้อมูลแยกกัน: 'mood_4' เก็บสี, 'note_4' เก็บบันทึก
+        final String? moodName = _box.get('mood_$key');
+        final String note = _box.get('note_$key') ?? '';
+
         if (moodName != null) {
-          loadedMoods[key] = MoodType.values.byName(moodName);
+          loadedData[key] = DayRecord(
+            mood: MoodType.values.byName(moodName),
+            note: note,
+          );
         }
       }
     }
-    state = loadedMoods;
+    state = loadedData;
   }
 
   void updateMood(int day, MoodType mood) {
-    _box.put(day, mood.name);
-    state = {...state, day: mood};
+    final currentNote = state[day]?.note ?? '';
+    _box.put('mood_$day', mood.name);
+
+    state = {...state, day: DayRecord(mood: mood, note: currentNote)};
+  }
+
+  void updateNote(int day, String note) {
+    final currentMood = state[day]?.mood;
+    _box.put('note_$day', note);
+
+    if (currentMood != null) {
+      state = {...state, day: DayRecord(mood: currentMood, note: note)};
+    }
   }
 }
 
 // Declear Provider valiable for useable in whole app
-final moodProvider = StateNotifierProvider<MoodNotifier, Map<int, MoodType>>((
+final moodProvider = StateNotifierProvider<MoodNotifier, Map<int, DayRecord>>((
   ref,
 ) {
   return MoodNotifier();

a89b6ee3c3302eddde168836e12b5414cb75256b
Xaysavanh-jojo <155747662+Xaysavanh-jojo@users.noreply.github.com>
2026-06-05 21:56:00 +0700

    Add data analytics of mood monthly

---
+++ lib/features/mood_tracker/presentation/widgets/mood_analytics_widget.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:pixel_mood_tracker/core/constants/app_colors.dart';
+
+class MoodAnalyticsWidget extends StatelessWidget {
+  final Map<int, MoodType> monthlyMoods;
+
+  const MoodAnalyticsWidget({super.key, required this.monthlyMoods});
+
+  @override
+  Widget build(BuildContext context) {
+    // check that the user has save any color mood on this month
+    if (monthlyMoods.isEmpty) {
+      return Text(
+        'No data recorded for this month yet.',
+        style: TextStyle(color: AppColors.textPrimary.withOpacity(0.5)),
+      );
+    }
+
+    // Count date for each mood
+    final Map<MoodType, int> moodCounts = {};
+    for (var mood in monthlyMoods.values) {
+      moodCounts[mood] = (moodCounts[mood] ?? 0) + 1;
+    }
+
+    final int totalRecordedDays = monthlyMoods.length;
+
+    return Column(
+      children:
+          MoodType.values.map((mood) {
+            final int count = moodCounts[mood] ?? 0;
+            final double percentage =
+                count / totalRecordedDays > 0
+                    ? (count / totalRecordedDays)
+                    : 0.0;
+
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 6.0),
+              child: Column(
+                // Show name mood and date count
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        mood.name.toUpperCase(),
+                        style: const TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.textPrimary,
+                        ),
+                      ),
+                      Text(
+                        '${(percentage * 100).toStringAsFixed(0)}% ($count days)',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: AppColors.textPrimary.withOpacity(0.6)
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 4),
+                  // Minimal progess bar
+                  Stack(
+                    children: [
+                      Container(
+                        height: 8,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.black12,
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                      ),
+                      // Flexible color tab
+                      FractionallySizedBox(
+                        widthFactor: percentage, // 0.0 - 1.0
+                        child: Container(
+                          height: 8,
+                          decoration: BoxDecoration(
+                            color: AppColors.getColorForMood(mood),
+                            borderRadius: BorderRadius.circular(4),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          }).toList(),
+    );
+  }
+}
